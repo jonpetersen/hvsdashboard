87d8198bb3bc77ca7db4e21133f3e818a1d23c5d
Jon Petersen <jonepetersen@gmail.com>
2016-08-02 13:15:33 +0100

    updated for end of July 2016

--- jobs/months_sales.rb
+++ jobs/months_sales.rb
@@ -1,13 +1,15 @@
-SCHEDULER.every '4m' do
+SCHEDULER.every '5m' do
 
   uri = URI('http://hvsapp.jonpetersen.co.uk/salesthismonth')
   current_salesthismonth = Net::HTTP.get(uri)    
   uri = URI('http://hvsapp.jonpetersen.co.uk/salesthismonthavg')
   current_salesthismonthavg = Net::HTTP.get(uri) 
   
-  days_left = Time.now.end_of_month.day - Date.today.day
+  days_left = Date.new(Time.now.year, Time.now.month, -1).day - Date.today.day
   
-  current_salesthismonthforecast = current_salesthismonth.to_f + (days_left + 1) * current_salesthismonthavg.to_f
+  days_left = days_left + 1 if Time.now.hour < 17
+  
+  current_salesthismonthforecast = current_salesthismonth.to_f + (days_left * current_salesthismonthavg.to_f)
 
   send_event('salesthismonth', { current: current_salesthismonth.to_i})
   send_event('salesthismonthavg', { current: current_salesthismonthavg.to_i})
--- jobs/todays_sales.rb
+++ jobs/todays_sales.rb
@@ -2,11 +2,10 @@ current_salesvalue = 0
 current_salesthismonth = 0
 current_saleslasttime = ""
 
-SCHEDULER.every '20s' do
-  last_salesvalue = current_salesvalue
-
-  uri = URI('http://hvsapp.jonpetersen.co.uk/salestoday')
-  current_salesvalue = Net::HTTP.get(uri)
+SCHEDULER.every '10s' do
+  last_salesvalue = YAML::load_file('tmp/todays_sales.yml')
+  last_salesvalue = 0 if Time.now.hour == 6
+  current_salesvalue = Net::HTTP.get(URI('http://hvsapp.jonpetersen.co.uk/salestoday'))
   uri = URI('http://hvsapp.jonpetersen.co.uk/salelasttime')
   current_saleslast = (Net::HTTP.get(uri))
   uri = URI('http://hvsapp.jonpetersen.co.uk/vattoday')
@@ -18,14 +17,20 @@ SCHEDULER.every '20s' do
     current_saleslastoperator = JSON.parse(current_saleslast)["operator"]
   end
     
-  uri = URI('http://hvsapp.jonpetersen.co.uk/transactionsrecent')
-  current_transactionsrecent = Net::HTTP.get(uri)
+  current_transactionsrecent = Net::HTTP.get(URI('http://hvsapp.jonpetersen.co.uk/transactionsrecent'))
   current_transactionscolour = "green"
   current_transactionscolour = "red" if current_transactionsrecent.to_i > 5 
 
+  if current_salesvalue.to_i > last_salesvalue
+    File.open('tmp/todays_sales.yml', 'w') {|f| f.write current_salesvalue.to_i.to_yaml } #Store
+  else
+    current_salesvalue = last_salesvalue
+  end
+  
   send_event('salesvalue', { current: current_salesvalue.to_i})
   send_event('lastsaletime', { current: current_saleslasttime})
   send_event('lastsaleoperator', { text: current_saleslastoperator + " - " + current_saleslasttime})
   send_event('transactionsrecent', { current: current_transactionsrecent, colour: current_transactionscolour})
   send_event('vattodaypercent', { current: current_vattodaypercent})
 end
+
