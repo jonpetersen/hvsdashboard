14c07ddbc40f5e6ce5abf545bc4533dca6ec21e1
Jon Petersen <jonepetersen@gmail.com>
2017-03-01 19:14:46 +0000

    updated for end of Feb 2017 data

--- jobs/todays_sales.rb
+++ jobs/todays_sales.rb
@@ -7,15 +7,15 @@ SCHEDULER.every '10s' do
   last_salesvalue = -1 if Time.now.hour == 6
   
   current_salesvalue = Net::HTTP.get(URI('http://hvsapp.jonpetersen.co.uk/salestoday'))
-  uri = URI('http://hvsapp.jonpetersen.co.uk/salelasttime')
-  current_saleslast = (Net::HTTP.get(uri))
-  uri = URI('http://hvsapp.jonpetersen.co.uk/vattoday')
-  current_vattodaypercent = (Net::HTTP.get(uri))
+  current_saleslast = Net::HTTP.get(URI('http://hvsapp.jonpetersen.co.uk/salelasttime'))
+  current_vattodaypercent = Net::HTTP.get(URI('http://hvsapp.jonpetersen.co.uk/vattoday'))
   if current_saleslast == "EOD"
 	current_saleslasttime = "EOD"
+    lastsaleoperator = "The Shop has closed"
   else
     current_saleslasttime = JSON.parse(current_saleslast)["time"]
     current_saleslastoperator = JSON.parse(current_saleslast)["operator"]
+    lastsaleoperator = current_saleslastoperator + " - " + current_saleslasttime
   end
     
   current_transactionsrecent = Net::HTTP.get(URI('http://hvsapp.jonpetersen.co.uk/transactionsrecent'))
@@ -24,7 +24,7 @@ SCHEDULER.every '10s' do
 
   if current_salesvalue.to_i > last_salesvalue
     File.open('tmp/todays_sales.yml', 'w') {|f| f.write current_salesvalue.to_i.to_yaml } #Store
-  elsif Time.now.hour > 23
+  elsif Time.now.hour > 22
     File.open('tmp/todays_sales.yml', 'w') {|f| f.write 0.to_yaml }
   else
     current_salesvalue = last_salesvalue
@@ -32,7 +32,7 @@ SCHEDULER.every '10s' do
   
   send_event('salesvalue', { current: current_salesvalue.to_i})
   send_event('lastsaletime', { current: current_saleslasttime})
-  send_event('lastsaleoperator', { text: current_saleslastoperator + " - " + current_saleslasttime})
+  send_event('lastsaleoperator', { text: lastsaleoperator})
   send_event('transactionsrecent', { current: current_transactionsrecent, colour: current_transactionscolour})
   send_event('vattodaypercent', { current: current_vattodaypercent})
 end
